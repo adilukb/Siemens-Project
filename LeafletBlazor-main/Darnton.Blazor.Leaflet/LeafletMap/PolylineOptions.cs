e07eca0da69efd554f8b295e3d3073f1e67c40c3
adilukb <bogdanadrian4@yahoo.com>
2022-05-20 17:53:17 +0300

    Initial Project w/out PopUp window

--- LeafletBlazor-main/Darnton.Blazor.Leaflet/LeafletMap/PolylineOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Blazor.Leaflet.OpenStreetMap.LeafletMap
-{
-    /// <summary>
-    /// The options used when creating a <see cref="Polyline"/>.
-    /// </summary>
-    public class PolylineOptions : LayerOptions
-    {
-    }
-}
